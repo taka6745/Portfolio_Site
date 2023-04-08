11dbc356774121146f71b1879fff74306ea0d024
taka6745 <takamundy@gmail.com>
2023-04-08 17:08:45 +1000

    SQL

--- u561601930_projects_mysql_create.sql
+++ u561601930_projects_mysql_create.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `projects` (
 	`Id` int(1000) NOT NULL AUTO_INCREMENT,
-	`Title` varchar(255) NOT NULL,
+	`Title` varchar(500) NOT NULL,
 	`Type` varchar(255) NOT NULL,
 	`Description` TEXT NOT NULL,
 	`Date_uploaded` DATE NOT NULL,
 	`Date_updated` DATE NOT NULL,
 	`Tags` varchar(255) NOT NULL,
-	`Image_link` varchar(255) NOT NULL,
+	`Image_link` varchar(1000) NOT NULL,
 	`Related_projects` varchar(255),
 	PRIMARY KEY (`Id`)
 );
