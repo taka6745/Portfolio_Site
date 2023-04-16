88bf21abe0a38d26c810067f783f329c3f4097d9
taka6745 <takamundy@gmail.com>
2023-04-17 08:49:45 +1000

    API access

--- portfolio/src/pages/ProjectBlock.js
+++ portfolio/src/pages/ProjectBlock.js
@@ -1,13 +1,71 @@
-import React from "react";
-import "./Layout.css";
-// Get from github, save all as physical json, update on /update
+import { useState, useEffect } from "react";
+import { Buffer } from "buffer";
 
-const ProjectBlock = ({ data }) => {
+function ProjectBlock({ data }) {
+  const [repoList, setRepoList] = useState([]);
+
+  useEffect(() => {
+    const fetchRepos = async () => {
+      try {
+        const username = "taka6745";
+        const url = `https://api.github.com/users/${username}/repos`;
+        const headers = {
+          Accept: "application/vnd.github.v3+json",
+          Authorization: "Bearer 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",
+        };
+        const response = await fetch(url, {
+          method: "GET",
+          headers: headers,
+        });
+        const repos = await response.json();
+        const repoNames = repos.map((repo) => repo.name);
+        setRepoList(repoNames);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchRepos();
+
+    const owner = "taka6745";
+    const repo = "Portfolio_Site";
+    const path = "portfolio.json";
+
+    // Set the API request headers
+    const headers = {
+      Accept: "application/vnd.github.v3+json",
+      Authorization: "Bearer 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",
+    };
+
+    // Set the API endpoint URL
+    const url = `https://api.github.com/repos/${owner}/${repo}/contents/${path}`;
+
+    // Make the API request
+    fetch(url, {
+      method: "GET",
+      headers: headers,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // Get the contents of the file from the API response
+        console.log("File created at: ", data[0].commit.author.date);
+        const fileContents = Buffer.from(data.content, "base64").toString();
+        const jsonContents = JSON.parse(fileContents);
+
+        // Print the contents of the file
+        console.log(jsonContents);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <>
-      <h1>THIS IS {data}</h1>
+      {repoList.map((repoName) => (
+        <h1 key={repoName}>THIS IS {repoName}</h1>
+      ))}
     </>
   );
-};
+}
 
 export default ProjectBlock;
---
+++ portfolio/src/pages/api.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+function projectBlock() {
+   
+    
+      return (
+      <>
+      
+        <h1>THIS IS {this.props.data}</h1>
+      </>
+    );
+  }
+
+  export default projectBlock;
+  
